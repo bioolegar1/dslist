33433938b0b5ca47533beed24ec0f90cc17d1e46
bioolegar1 <bioolegari@gmail.com>
2025-08-18 22:40:32 -0300

    limpeza do arquivo inicial

--- src/main/java/com/olegari/dslist/DslistApplication.java
+++ src/main/java/com/olegari/dslist/DslistApplication.java
@@ -11,20 +11,3 @@ public class DslistApplication {
 	}
 
 }
-bioolegari@bioolegari-Nitro-ANV15-51:~/Documentos/projeto/dslist$ git init
-hint: Using 'master' as the name for the initial branch. This default branch name
-hint: is subject to change. To configure the initial branch name to use in all
-hint: of your new repositories, which will suppress this warning, call:
-hint:
-hint:   git config --global init.defaultBranch <name>
-hint:
-hint: Names commonly chosen instead of 'master' are 'main', 'trunk' and
-hint: 'development'. The just-created branch can be renamed via this command:
-hint:
-hint:   git branch -m <name>
-Repositório vazio Git inicializado em  /home/bioolegari/Documentos/projeto/dslist/.git/
-bioolegari@bioolegari-Nitro-ANV15-51:~/Documentos/projeto/dslist$ git add .
-        warning: in the working copy of 'mvnw.cmd', LF will be replaced by CRLF the next time Git touches it
-bioolegari@bioolegari-Nitro-ANV15-51:~/Documentos/projeto/dslist$ git commit "Project Created"
-error: pathspec 'Project Created' did not match any file(s) known to git
-bioolegari@bioolegari-Nitro-ANV15-51:~/Documentos/projeto/dslist$
